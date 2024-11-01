edc116ee3379b50d40b45e9cf266806c9c60ceb6
JhoahoSanchez <jhoaho.dev@gmail.com>
2024-11-01 08:46:00 -0500

    Modificando el JDK

--- src/main/java/com/sideralsoft/Main.java
+++ src/main/java/com/sideralsoft/Main.java
@@ -4,7 +4,6 @@ import com.sideralsoft.config.Scheduler;
 
 public class Main {
     public static void main(String[] args) {
-        //get("/helloworld", (req, res) -> "Hello World");
 //        DetalleCertificados d = new DetalleCertificados();
 //        d.comprobarValidezCertificados();
         Scheduler scheduler = new Scheduler();
